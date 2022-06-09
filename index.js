80707234fde18be0b42278c70d6fd6afa54cf819
yugmani <yugmani@yahoo.com>
2022-06-08 23:07:58 -0700

    creating a basic promise with and without function

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 // Import stylesheets
 import './style.css';
 
+// *******************************************
+// ********** creating a promise *************
+// *******************************************
+
+const promise1 = new Promise(function (resolve, reject) {
+  //Resolve the Promise passing a message as a data
+  resolve('Success: promise resolved.');
+
+  //if some error occurs
+  if (error) {
+    //Reject the promise passing a message as a data
+    reject('Failure: promise rejected.');
+  }
+});
+
+//invoke the promise
+console.log(promise1);
+//Promise {}
+
+//Returning a promise from a function
+// *************************************************
+
+function myPromiseFunction() {
+  return new Promise(function (resolve, reject) {
+    //Resolve the Promise passing a message as a data
+    resolve('Success: promise resolved.');
+
+    //if some error occurs
+    if (error) {
+      //Reject the promise passing a message as a data
+      reject('Failure: promise rejected.');
+    }
+  });
+}
+
+//invoke myPromiseFunction() to invoke the promise inside it.
+console.log(myPromiseFunction());
+// Promise {}
+
